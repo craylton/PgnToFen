848be50a1017ab05f62c3b6f27a1dbdf5d5c5987
Simon Palmer <sipalmer123@gmail.com>
2019-10-17 21:22:11 +0100

    Provide whose turn it is to move in the full conversion strategy

--- PgnToFenCore/FenData.cs
+++ PgnToFenCore/FenData.cs
@@ -5,20 +5,20 @@ namespace PgnToFenCore
     public class FenData
     {
         public string Fen { get; }
-
         public int MoveNumber { get; }
-
         public FinalGameResult FinalResult { get; }
+        public bool IsWhiteToMove { get; }
 
-        public FenData(string fen, int moveNumber, FinalGameResult finalResult) => 
-            (Fen, MoveNumber, FinalResult) = (fen, moveNumber, finalResult);
+        public FenData(string fen, int moveNumber, FinalGameResult finalResult, bool isWhiteToMove) =>
+            (Fen, MoveNumber, FinalResult, IsWhiteToMove) = (fen, moveNumber, finalResult, isWhiteToMove);
 
         public static FenData FromGameState(ChessGame game, FinalGameResult endResult)
         {
             var fen = game.GetFen();
             var moveNumber = game.FullMoveNumber;
+            var isWhiteToMove = game.WhoseTurn == Player.White;
 
-            return new FenData(fen, moveNumber, endResult);
+            return new FenData(fen, moveNumber, endResult, isWhiteToMove);
         }
     }
 }
